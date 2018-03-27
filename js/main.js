2594e19b93bd4435d8439a1685f3b30c860c8356
Marta Aguilar <martaguilar.soto@gmail.com>
2018-03-28 00:45:20 +0200

    open and close menu nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+//ocultar texto menu del boton
+var textMenu = document.querySelector('.nav-trigger');
+var textClose = document.querySelector('.nav-close');
+textMenu.innerHTML = '';
+textClose.innerHTML = '';
+
+var btnOpen = document.querySelector('.nav-trigger');
+var btnClose = document.querySelector('.nav-close');
+
+btnOpen.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+
+function openMenu(){
+  var navMenu = document.querySelector('.nav');
+  navMenu.style.display = 'block';
+  btnOpen.style.display = 'none';
+}
+
+function closeMenu(){
+  var navMenu = document.querySelector('.nav');
+  navMenu.style.display = 'none';
+  btnOpen.style.display = 'block';
+
+}
+
+function hiddenItemNav(){
+var navItem = document.querySelector('.nav');
+  navItem.style.display = 'none';
+}
+window.addEventListener('load', hiddenItemNav);
